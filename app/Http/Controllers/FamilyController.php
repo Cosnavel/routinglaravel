fb5157323d310590e1aabacda70429f1d6561385
Cosnavel <niclasdevelop@gmail.com>
2020-05-12 09:52:05 +0200

    fixed

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Family;
 use App\Http\Requests\FamilyRequest;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class FamilyController extends Controller
 {
